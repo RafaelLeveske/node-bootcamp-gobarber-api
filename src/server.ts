4f049e572a352ebde70dc76a0e961e20cfa66b81
Rafael Vieira <rafaelleveske@gmail.com>
2020-06-23 10:53:40 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+import routes from './routes'
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server Online')
+});
