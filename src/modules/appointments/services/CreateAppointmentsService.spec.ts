64a1729b6574524733945c75a5ea00f4b1ddc162
Rafael Vieira <rafaelleveske@gmail.com>
2020-12-02 14:01:05 -0300

    Criando o primeiro teste

--- src/modules/appointments/services/CreateAppointmentsService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentsService.spec.ts
@@ -1,7 +1,18 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('deve ser permitido criar um agendamento', () => {
-    expect(1 + 2).toBe(3);
+  it('deve ser permitido criar um agendamento', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
   });
 });
