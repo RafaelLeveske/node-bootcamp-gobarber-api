69e4967ec648e1a296ca96530d5dd2318424f450
Rafael Vieira <rafaelleveske@gmail.com>
2020-12-03 11:25:40 -0300

    Reset de senha

---
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -0,0 +1,39 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import ResetPasswordService from './ResetPasswordService';
+import FakeUserTokensRepository from '../repositories/fakes/FakeUserTokensRepository';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeUserTokensRepository: FakeUserTokensRepository;
+let resetPasswordService: ResetPasswordService;
+
+describe('ResetPasswordService', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeUserTokensRepository = new FakeUserTokensRepository();
+
+    resetPasswordService = new ResetPasswordService(
+      fakeUsersRepository,
+      fakeUserTokensRepository,
+    );
+  });
+
+  it('deve ser permitido resetar a senha', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Rafael',
+      email: 'rafael@example.com',
+      password: '123456',
+    });
+
+    const { token } = await fakeUserTokensRepository.generate(user.id);
+
+    await resetPasswordService.execute({
+      password: '123123',
+      token,
+    });
+
+    const updatedUser = await fakeUsersRepository.findById(user.id);
+
+    expect(updatedUser?.password).toBe('123123');
+  });
+});
